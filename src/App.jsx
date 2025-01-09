fbcf000e090ba6d172e05c6b8b9bbf3adf4fd424
ForgeBrew96 <christianbmendoza96@gmail.com>
2025-01-09 14:23:57 -0500

    motion-react styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,62 @@
-import { useState } from 'react'
+import { motion, useTime, useTransform } from "motion/react"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const time = useTime();
+const rotate = useTransform(time, [0, 4000], [0, 360], { clamp: false });
 
   return (
     <>
-      <h1>Christian's Portfolio</h1>
-      <div className="portfolio-grid">
-        <div className="pgrid-1">
+     <div className="special-box">
+      <motion.div style={{ rotate }} />
+    </div>
+      <motion.h1
+       initial={{ opacity: 0, scale: 0 }}
+       animate={{ opacity: 1, scale: 1 }}
+       transition={{
+           duration: 0.4,
+           scale: { type: "spring", visualDuration: 0.7, bounce: 0.3 },
+       }}
+       className="title"
+      >Christian's Portfolio</motion.h1>
+      <motion.div 
+        initial={{ opacity: 0 }}
+        whileHover={{ backgroundColor: "rgb(68, 130, 146)" }}
+        whileInView={{ opacity: 1 }}
+      className="portfolio-grid">
+        <motion.div initial={{scale: 1}}
+         whileHover={{ scale: 1.1 }}
+         whileTap={{ scale: 0.95, backgroundColor: "rgb(73, 73, 73)" }}
+         onHoverStart={() => console.log('hover started!')}
+         className="pgrid-1 pgrids">
           Project 1
-        </div>
-        <div className="pgrid-2">
+        </motion.div>
+        <motion.div
+        initial={{scale: 1}}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95, backgroundColor: "rgb(73, 73, 73)" }}
+        onHoverStart={() => console.log('hover started!')} 
+        className="pgrid-2 pgrids">
           Project 2
-        </div>
-        <div className="pgrid-3">
+        </motion.div>
+        <motion.div 
+        initial={{scale: 1}}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95, backgroundColor: "rgb(73, 73, 73)" }}
+        onHoverStart={() => console.log('hover started!')}
+        className="pgrid-3 pgrids">
           Project 3
-        </div>
-        <div className="pgrid-4">
+        </motion.div>
+        <motion.div 
+        initial={{scale: 1}}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95, backgroundColor: "rgb(73, 73, 73)" }}
+        onHoverStart={() => console.log('hover started!')}
+        className="pgrid-4 pgrids">
           Project 4
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </>
   )
 }
